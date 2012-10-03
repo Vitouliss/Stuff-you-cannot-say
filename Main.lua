9e18010a50d8d10e4816607520a1264f1afe09ac
Vitouliss14 <vitouliss14@yahoo.com>
2012-10-03 09:44:39 -0300

    Formatted it in Sublime text 2

--- Main.lua
+++ Main.lua
@@ -1,23 +1,23 @@
 local stuffyoucannotsay = {"retard","stfu"}
 
 function NMes(thing)
-m = Instance.new("Message",Workspace)
-m.Text = thing
-wait(3)
-m:Destroy()
-end 
+  m = Instance.new("Message",Workspace)
+	m.Text = thing
+	wait(3)
+	m:Destroy()
+end
 
 game:GetService("Players").PlayerAdded:connect(function(noob)
-noob.Chatted:connect(function(chat)
+	noob.Chatted:connect(function(chat)
 
-for a,b in pairs(stuffyoucannotsay) do 
+	for a,b in pairs(stuffyoucannotsay) do
 
-if string.lower(chat):find(b) then
-NMes(noob.Name.." SAID SOMETHING BAD!")
+		if string.lower(chat):find(b) then
+			NMes(noob.Name.. " SAID SOMETHING BAD!")
 
---Do what you want to the person here
+			-- Do what you want to the person here
 
-end
-end
-end)
-end)
+		end
+	end
+	end)
+	end)
