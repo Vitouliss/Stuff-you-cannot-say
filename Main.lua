6a29e4210f409a21b0361aebdc5be3162d022402
MoaufmKlo <45636897+MonurMo@users.noreply.github.com>
2019-10-15 11:50:16 +0200

    Renamed variables, moved NMes function's content into the message event and did some formatting
    The NMes function was unnessecrary because it was only used once, therefore I moved it's content into the message event.
    
    Did some spacing and renamed some variables.

--- Main.lua
+++ Main.lua
@@ -1,23 +1,18 @@
-local stuffyoucannotsay = {"retard","stfu"}
+local stuffyoucannotsay = {"retard", "stfu"}
 
-function NMes(thing)
-  m = Instance.new("Message",Workspace)
-	m.Text = thing
-	wait(3)
-	m:Destroy()
-end
-
-game:GetService("Players").PlayerAdded:connect(function(noob)
-	noob.Chatted:connect(function(chat)
-
-	for a,b in pairs(stuffyoucannotsay) do
-
-		if string.lower(chat):find(b) then
-			NMes(noob.Name.. " SAID SOMETHING BAD!")
-
-			-- Do what you want to the person here
-
+game:GetService("Players").PlayerAdded:Connect(function(player)
+	player.Chatted:Connect(function(msgContent)
+		for key, value in pairs(stuffyoucannotsay) do
+			if string.lower(msgContent):Find(value) then
+				
+				local message = Instance.new("Message", game.Workspace)
+				message.Text = player.Name .. " SAID SOMETHING BAD!"
+				wait(3)
+				message:Destroy()
+				
+				-- Do what you want to the player here
+				
+			end
 		end
-	end
 	end)
-	end)
+end)
